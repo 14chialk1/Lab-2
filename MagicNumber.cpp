4770d186a0e5037def3cafa6795df43259f4afd6
14chialk1 <30090534@cc.peralta.edu>
2026-02-09 11:59:28 -0800

    Implement magic number calculation in MagicNumber.cpp

---
+++ MagicNumber.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int favourite_integer;
+    cout << "Write down your favourite integer!"<< endl;
+    cin >> favourite_integer;
+    magic_performed_favourite_integer = ((2*favourite_integer+10)/2)-favourite_integer
+    cout << "Nutters! The favourite integer is  " << magic_performed_favourite_integer << endl;
+    return 0;
+}
